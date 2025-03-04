5ddfc3bfbde9e6f58ccfb8542e8adb1f4b3b6ec5
David Leigh <dleigh@us.ibm.com>
2025-03-04 16:07:59 -0500

    add influxdb

--- temp_logger.py
+++ temp_logger.py
@@ -6,6 +6,8 @@ from max31855 import MAX31855, MAX31855Error
 import librato
 import yaml
 import os
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client .client.write_api import SYNCHRONOUS
 
 class MAX31855a(MAX31855):
     def get(self):
@@ -27,12 +29,11 @@ logger = logging.getLogger()
 logger.addHandler(log_handler)
 logger.setLevel(logging.INFO)
 
-api = librato.connect(config['librato_email'], config['librato_key'])
 cs_pins = []
 pin_mapping = {}
 for thermocouple in config['thermocouples']:
   cs_pins.append(thermocouple['pin'])
-  pin_mapping[thermocouple['pin']] = thermocouple['name']
+  pin_mapping[thermocouple['pin']] = {'name': thermocouple['name'], 'location': thermocouple['location']}
 clock_pin = config['clock_pin']
 data_pin = config['data_pin']
 frequency = config['frequency']
@@ -44,19 +45,27 @@ for cs_pin in cs_pins:
 running = True
 while(running):
     try:
-        q   = api.new_queue()
+        librato_api = librato.connect(config['librato_email'], config['librato_key'])
+        librato_queue = librato_api.new_queue()
+        influxdb_client = InfluxDBClient(url=config['influxdb_url'], token=config['influxdb_token'], org=config['influxdb_org'])
+        write_api = influxdb_client.write_api(write_options=SYNCHRONOUS)
+        points = []
         for thermocouple in thermocouples:
             rj = thermocouple.get_rj()
-            name = pin_mapping[thermocouple.cs_pin]
+            name = pin_mapping[thermocouple.cs_pin]['name']
+            location = pin_mapping[thermocouple.cs_pin]['location'] 
             tc = thermocouple.get()
             logging.info("source: %s temp: %s" % (name, tc))
-            q.add('temperature',tc,source=name)
+            librato_queue.add('temperature',tc,source=name)
+            point = Point("oven-temp").tag("location", location).tag("name",name).field("temperature", tc)
+            points.append(point)
             time.sleep(0.5)
-        q.submit()
+        librato_queue.submit()
+        write_api.write(bucket=config['influxdb_bucket'], record=points)
         time.sleep(frequency)
     except KeyboardInterrupt:
         running = False
-    except:
-        logging.error("some type of error occurred!")
+    except Exception as e:
+        logging.error("Error: %s" % e)
 for thermocouple in thermocouples:
     thermocouple.cleanup()
